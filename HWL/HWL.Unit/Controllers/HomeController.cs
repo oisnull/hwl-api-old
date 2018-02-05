48a4e56e942c948ff0cac85f331c2aeb4211b582
1577592799 <1577592799@qq.com>
2018-02-05 21:01:42 +0800

    add unit test

---
+++ HWL/HWL.Unit/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using HWL.Unit.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HWL.Unit.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            RabbitMQ.MQManager.SendMessage("user-2-queue", GetAddFriendBean());
+
+            return View();
+        }
+
+        public byte[] GetAddFriendBean()
+        {
+            var model = new AddFriendBean()
+            {
+                friendId = 2,
+                remark = "我是 2536",
+                userHeadImage = "http://192.168.1.4:8033//upload/user-head/2018//2018012613243120180126212432.jpg",
+                userId = 1,
+                userName = "2536"
+            };
+
+            String json = JsonConvert.SerializeObject(model);
+            return Encoding.UTF8.GetBytes(json);
+        }
+    }
+}
---
+++ HWL/HWL.Unit/Models/AddFriendBean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWL.Unit.Models
+{
+    public class AddFriendBean
+    {
+        public int userId { get; set; }
+        public String userName { get; set; }
+        public String userHeadImage { get; set; }
+        public int friendId { get; set; }
+        public String remark { get; set; }
+    }
+}

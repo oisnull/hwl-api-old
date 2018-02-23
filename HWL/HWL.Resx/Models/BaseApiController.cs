d9ede36e0e1fa391df9f80615e3a0321cd1cb568
o is null <1577592799@qq.com>
2018-02-23 16:39:31 +0800

    123

---
+++ HWL/HWL.Resx/Models/BaseApiController.cs
@@ -0,0 +1,55 @@
+﻿using GMSF.Model;
+using HWL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HWL.Resx.Models
+{
+    public class BaseApiController : ApiController
+    {
+        //protected bool CheckToken(string token)
+        //{
+        //    RequestValidate requestValidate = new RequestValidate();
+        //    return requestValidate.CheckToken(token);
+        //}
+        //protected Response<UpImageResponseBody> GetResult(string resultCode, string message, UpImageResponseBody body = null)
+        //{
+        //    Response<UpImageResponseBody> response = new Response<UpImageResponseBody>();
+        //    response.Head = new GMSF.HeadDefine.ResponseHead()
+        //    {
+        //        ResultCode = resultCode,
+        //        ResultMessage = message
+        //    };
+        //    response.Body = body;
+
+        //    return response;
+        //}
+
+        /// <summary>
+        /// 返回元祖《表示验证是否成功,验证成功后返回的userid》
+        /// </summary>
+        protected Tuple<bool, int> CheckToken(string token)
+        {
+            RequestValidate requestValidate = new RequestValidate();
+            bool succ = requestValidate.CheckToken(token);
+            int userid = requestValidate.GetCurrUserId();
+            return new Tuple<bool, int>(succ, userid);
+        }
+
+        protected Response<ResxResult> GetResult(string resultCode, string message, ResxResult body = null)
+        {
+            Response<ResxResult> response = new Response<ResxResult>();
+            response.Head = new GMSF.HeadDefine.ResponseHead()
+            {
+                ResultCode = resultCode,
+                ResultMessage = message
+            };
+            response.Body = body;
+
+            return response;
+        }
+    }
+}
---
+++ HWL/HWL.Resx/Models/ResxConfigManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+
+namespace HWL.Resx.Models
+{
+    public class ResxConfigManager
+    {
+        /// <summary>
+        /// 文件上传目录配置,值为当前计算机的绝对地址
+        /// </summary>
+        public static string UploadDirectory
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["UploadDirectory"];
+            }
+        }
+
+        /// <summary>
+        /// 文件对外访问的地址配置,值以http://或者https://开头
+        /// </summary>
+        public static string FileAccessUrl
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileAccessUrl"];
+            }
+        }
+
+        /// <summary>
+        /// 定义图片允许上传的文件扩展名
+        /// </summary>
+        public readonly static string[] IMAGE_FILE_TYPES = { "gif", "jpg", "jpeg", "png", "bmp" };
+        public readonly static int IMAGE_MAX_SIZE = 5 * 1024 * 1024;//默认5M
+
+        /// <summary>
+        /// 定义语音允许上传的文件扩展名
+        /// </summary>
+        public readonly static string[] SOUND_FILE_TYPES = { "amr", "wav" };
+        public readonly static int SOUND_MAX_TIME = 60;//默认60s
+
+        /// <summary>
+        /// 定义视频允许上传的文件扩展名
+        /// </summary>
+        public readonly static string[] VIDEO_FILE_TYPES = { "mp4" };
+        public readonly static int VIDEO_MAX_SIZE = 20 * 1024 * 1024;//默认20M
+    }
+}

a067a90bfecf7da588096f7b7fd148fbdb3b20bb
o is null <1577592799@qq.com>
2018-05-05 19:11:35 +0800

    group action is complete

--- HWL/HWL.Service/Group/Service/DeleteGroupUser.cs
+++ HWL/HWL.Service/Group/Service/DeleteGroupUser.cs
@@ -28,18 +28,42 @@ namespace HWL.Service.Group.Service
             DeleteGroupUserResponseBody res = new DeleteGroupUserResponseBody() { Status = ResultStatus.Failed };
             using (HWLEntities db = new HWLEntities())
             {
-                var user = db.t_group_user.Where(u => u.group_guid == this.request.GroupGuid && u.user_id == this.request.UserId).FirstOrDefault();
-                if (user == null)
+                var group = db.t_group.Where(g => g.group_guid == this.request.GroupGuid).FirstOrDefault();
+                if (group != null)
                 {
-                    new Redis.GroupAction().DeleteGroupUser(this.request.GroupGuid, this.request.UserId);
-                    res.Status = ResultStatus.Success;
-                    return res;
+                    var grouUser = db.t_group_user.Where(u => u.group_guid == this.request.GroupGuid && u.user_id == this.request.UserId).FirstOrDefault();
+                    if (grouUser != null)
+                    {
+                        db.t_group_user.Remove(grouUser);
+                        group.group_user_count = group.group_user_count - 1;
+                        if (group.group_user_count < 0) group.group_user_count = 0;
+                    }
+                    if (group.build_user_id == this.request.UserId)
+                    {
+                        group.build_user_id = 0;
+                    }
+                    db.SaveChanges();
                 }
 
-                db.t_group_user.Remove(user);
-                db.SaveChanges();
                 new Redis.GroupAction().DeleteGroupUser(this.request.GroupGuid, this.request.UserId);
                 res.Status = ResultStatus.Success;
+
+                try
+                {
+                    if (new Redis.GroupAction().GetGroupUserCount(this.request.GroupGuid) > 0) return res;
+
+                    //如果redis的群组里没有人了，就直接删除组
+                    var users = db.t_group_user.Where(g => g.group_guid == this.request.GroupGuid).ToList();
+                    if (users != null && users.Count > 0)
+                    {
+                        if (group != null) db.t_group.Remove(group);
+                        db.t_group_user.RemoveRange(users);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception)
+                {
+                }
             }
             return res;
         }
